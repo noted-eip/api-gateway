06e3b0772c028008c5d6f8693610730dbbf97a8a
Maxime Dodin <maxime.dodin@epitech.eu>
2022-05-02 20:53:17 +0200

    refacto: put route rest/n/:name handler into account service (account.go), calling createAccount method

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 
 	pb "api-gateway/grpc/accountspb"
 
@@ -14,34 +12,17 @@ import (
 func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	AccountClient := pb.NewAccountsServiceClient(conn)
+
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	AccountClient := pb.NewAccountsServiceClient(conn)
 
 	// Set up a http server.
 	r := gin.Default()
-	r.GET("/rest/n/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		id := "none"
-		email := "maxime@noted.io"
 
-		// Contact the server and print out its response.
-		req := &pb.Account{Id: id, Name: name, Email: email}
-		res, err := AccountClient.CreateAccount(c, req)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"result": fmt.Sprint(res),
-		})
-
-	})
+	r.GET("/rest/n/:name", account(AccountClient))
 
 	// Run http server
 	if err := r.Run(":8052"); err != nil {
