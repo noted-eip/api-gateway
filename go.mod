e918b54e9443ce331a97b9c1ef83cb5b14bc2b17
Diego ROJAS <rojasdiegopro@gmail.com>
2023-02-04 22:58:04 +0700

    feat: grpc gateway error handler

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0
+	go.uber.org/zap v1.24.0
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
 )
@@ -11,10 +12,8 @@ require (
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,7 +13,9 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -31,20 +34,28 @@ const (
 
 func main() {
 	kingpin.MustParse(app.Parse(os.Args[1:]))
-
-	logger := initLogger()
-
+	srv := newServer()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	mux := runtime.NewServeMux()
+	// Register gRPC APIs here.
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	must(accountsv1.RegisterAccountsAPIHandlerFromEndpoint(ctx, srv.mux, *accountsServiceAddress, opts))
+	must(notesv1.RegisterGroupsAPIHandlerFromEndpoint(ctx, srv.mux, *notesServiceAddress, opts))
 
-	must(accountsv1.RegisterAccountsAPIHandlerFromEndpoint(ctx, mux, *accountsServiceAddress, opts))
-	must(notesv1.RegisterGroupsAPIHandlerFromEndpoint(ctx, mux, *notesServiceAddress, opts))
+	srv.run()
+}
 
-	logger.Info("starting api-gateway", zap.Int16("port", *port))
-	must(http.ListenAndServe(fmt.Sprint(":", *port), mux))
+type server struct {
+	logger *zap.Logger
+	mux    *runtime.ServeMux
+}
+
+func newServer() *server {
+	srv := &server{}
+	srv.initLogger()
+	srv.mux = runtime.NewServeMux(runtime.WithErrorHandler(srv.errorHandler))
+	return srv
 }
 
 func must(err error) {
@@ -53,14 +64,44 @@ func must(err error) {
 	}
 }
 
-func initLogger() *zap.Logger {
+func (srv *server) initLogger() {
+	var err error
 	if *environment == envIsDev {
-		logger, err := zap.NewDevelopment()
+		srv.logger, err = zap.NewDevelopment()
 		must(err)
-		return logger
+	} else {
+		srv.logger, err = zap.NewProduction()
+		must(err)
 	}
-	logger, err := zap.NewProduction()
-	must(err)
+}
 
-	return logger
+type httpError struct {
+	Error string `json:"error"`
+}
+
+func (srv *server) errorHandler(ctx context.Context, sm *runtime.ServeMux, m runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+	st, ok := status.FromError(err)
+	if !ok {
+		srv.logger.Error("service replied with non-status error", zap.String("path", r.URL.Path), zap.String("method", r.Method))
+		st = status.New(codes.Internal, "internal server error")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	httpStatus := runtime.HTTPStatusFromCode(st.Code())
+	w.WriteHeader(httpStatus)
+
+	bytes, err := json.Marshal(httpError{Error: st.Message()})
+	if err != nil {
+		srv.logger.Error("failed to marshal error response", zap.Any("status", st), zap.Error(err))
+		return
+	}
+
+	if _, err := w.Write(bytes); err != nil {
+		srv.logger.Error("failed to write error response", zap.Any("status", st), zap.Error(err))
+	}
+}
+
+func (srv *server) run() {
+	srv.logger.Info("starting api-gateway", zap.Int16("port", *port))
+	must(http.ListenAndServe(fmt.Sprint(":", *port), srv.mux))
 }
