654900c954c4a85ea4c93e0f644bab60edaee5ac
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-10-31 10:17:31 +0100

    feat: New CreateNote handler fix: writeError fix double http.Error feat: update modules

--- notes.go
+++ notes.go
@@ -4,6 +4,7 @@ import (
 	notesv1 "api-gateway/protorepo/noted/notes/v1"
 	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"time"
@@ -11,6 +12,58 @@ import (
 
 type notesHandler struct {
 	notesClient notesv1.NotesAPIClient
+}
+
+func (h *notesHandler) CreateNote(w http.ResponseWriter, r *http.Request, pathParams map[string]string) /*(code int, contentType string, data []byte)*/ {
+	bearer, err := authenticate(r)
+	if err != nil {
+		writeError(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	// Read body and interpret it in a Create Note request struct
+	var body notesv1.CreateNoteRequest
+	err = json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		err := errors.New("wrong body: " + err.Error())
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// Fetch group ID to complete Create Note request
+	groupId, ok := pathParams["group_id"]
+	if !ok {
+		err := errors.New("group id error: " + err.Error())
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+	body.GroupId = groupId
+
+	// Fetch language to complete Create Note request
+	lang := r.URL.Query().Get("lang")
+	if lang == "" {
+		err := errors.New("expect note's language")
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+	body.Lang = lang
+
+	// Call the correct endpoint
+	res, err := h.notesClient.CreateNote(contextWithGrpcBearer(context.Background(), bearer), &body)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// Marshal result to JSON and respond
+	resBytes, err := json.Marshal(res)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resBytes)
 }
 
 func (h *notesHandler) ExportNote(w http.ResponseWriter, r *http.Request, pathParams map[string]string) /*(code int, contentType string, data []byte)*/ {
